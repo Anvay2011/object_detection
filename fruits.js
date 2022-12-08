f863632359ceddd72c92d38d00ccd63cb67fa035
Anvay2011 <83815773+Anvay2011@users.noreply.github.com>
2022-12-08 17:43:27 +0530

    Add files via upload

---
+++ fruits.js
@@ -0,0 +1,47 @@
+img="";
+objects=[];
+status="";
+function preload(){
+    img=loadImage('fruits_2.jpg');
+}
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+
+    objectDetector=ml5.objectDetector("Cocossd",modelLoaded);
+
+    document.getElementById("status").innerHTML="Status:Detecting Objects";
+}
+function draw(){
+    image(img,0,0,640,420);
+   
+    if(status!=""){
+        for(i=0; i<objects.length; i++){
+            document.getElementById("status").innerHTML="Status:Objects Detected";
+            fill("red");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%",objects[i].x+10,objects[i].y+10);
+            noFill();
+            stroke("red");
+            rect(objects[i].x-10,objects[i].y-15,objects[i].width,objects[i].height-5);
+        }
+    }
+}
+
+function modelLoaded(){
+    console.log("model Loaded");
+    status=true;
+    objectDetector.detect(img,gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects=results;
+}
+
+function back(){
+    window.location = "index.html";
+}
---
+++ main.js
@@ -0,0 +1,12 @@
+function bottle_location(){
+    window.location = "bottle.html";
+}
+function fam_location(){
+    window.location = "fam.html";
+}
+function fruits_location(){
+    window.location = "fruits.html";
+}
+function tv_location(){
+    window.location = "tv.html";
+}
